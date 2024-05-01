87e35c05ebc6af5533e676ee9405c279acb6aef3
rasmusmeinert <77355900+rasmusmeinert@users.noreply.github.com>
2024-05-01 09:51:22 +0200

    Controller - Udflugt, Hotel, HotelTilvalg, Konference
    rettelse i hotel og konference, med nødvendige metoder

--- src/main/java/model/Hotel.java
+++ src/main/java/model/Hotel.java
@@ -8,10 +8,15 @@ public class Hotel {
     private double prisEnkelt;
     private double prisDobbelt;
     private final ArrayList<Booking> bookinger = new ArrayList<>();
+    private final ArrayList<HotelTilvalg> hotelTilvalg = new ArrayList<>();
 
     public Hotel(String navn, double prisEnkelt, double prisDobbelt) {
         this.navn = navn;
         this.prisEnkelt = prisEnkelt;
         this.prisDobbelt = prisDobbelt;
+    }
+
+    public void tilføjHotelTilvalg(HotelTilvalg hotelTilvalg){
+       this.hotelTilvalg.add(hotelTilvalg);
     }
 }
