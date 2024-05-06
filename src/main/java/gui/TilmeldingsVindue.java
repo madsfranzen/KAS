dc7e7c4f150fd100e2545b425d9d1fda174a745d
rasmusmeinert <77355900+rasmusmeinert@users.noreply.github.com>
2024-05-06 11:43:53 +0200

    Update TilmeldingsVindue.java

--- src/main/java/gui/TilmeldingsVindue.java
+++ src/main/java/gui/TilmeldingsVindue.java
@@ -15,28 +15,29 @@ import storage.Storage;
 
 public class TilmeldingsVindue extends Application {
 
-    Konference konference;
+    private Konference konference;
 
-    DatePicker dpCheckIn = new DatePicker();
-    DatePicker dpCheckUd = new DatePicker();
-    DatePicker dpDeltagerFra = new DatePicker();
-    DatePicker dpDeltagerTil = new DatePicker();
+    private DatePicker dpCheckIn = new DatePicker();
+    private DatePicker dpCheckUd = new DatePicker();
+    private DatePicker dpDeltagerFra = new DatePicker();
+    private DatePicker dpDeltagerTil = new DatePicker();
 
-    CheckBox cbxHotelØnskes = new CheckBox("Hotel Ønskes");
+    private CheckBox cbxHotelØnskes = new CheckBox("Hotel Ønskes");
 
-    ListView lvwHoteller = new ListView();
-    ListView lvwHotelTilvalg = new ListView();
+    private ListView lvwHoteller = new ListView();
+    private ListView lvwHotelTilvalg = new ListView();
 
-    TextArea txaHotelInfo = new TextArea();
+    private TextArea txaHotelInfo = new TextArea();
 
-    CheckBox cbxForedragsholder = new CheckBox("Foredragsholder");
-    CheckBox cbxLedsager = new CheckBox("Ledsager");
+    private CheckBox cbxForedragsholder = new CheckBox("Foredragsholder");
+    private CheckBox cbxLedsager = new CheckBox("Ledsager");
 
-    TextField txfLedsagerNavn = new TextField();
+    private TextField txfLedsagerNavn = new TextField();
 
-    ListView lvwUdflugter = new ListView();
+    private ListView lvwUdflugter = new ListView();
 
-    TextField txfSamletPris = new TextField();
+    private TextField txfSamletPris = new TextField();
+
     @Override
     public void start(Stage stage) {
         stage.setTitle("KAS");
@@ -49,88 +50,97 @@ public class TilmeldingsVindue extends Application {
         stage.show();
     }
 
-    public void initContent(GridPane pane){
+    public void initContent(GridPane pane) {
         pane.setPadding(new Insets(20));
         pane.setVgap(20);
         pane.setHgap(20);
-        pane.setMaxSize(800,470);
+        pane.setMaxSize(800, 470);
 //        pane.setGridLinesVisible(true);
-
 
 
         //================ Hotel Information ======================
         Label lblCheckIn = new Label("Check In:");
         Label lblCheckUd = new Label("Check Ud:");
-        pane.add(lblCheckIn,0,0);
-        pane.add(lblCheckUd,1,0);
-        pane.add(dpCheckIn,0,1);
-        pane.add(dpCheckUd,1,1);
+        pane.add(lblCheckIn, 0, 0);
+        pane.add(lblCheckUd, 1, 0);
+        pane.add(dpCheckIn, 0, 1);
+        pane.add(dpCheckUd, 1, 1);
         dpCheckIn.setMaxWidth(115);
         dpCheckUd.setMaxWidth(115);
+        dpCheckIn.setDisable(true);
+        dpCheckUd.setDisable(true);
 
-        pane.add(cbxHotelØnskes,2,1);
+        pane.add(cbxHotelØnskes, 2, 1);
+        cbxHotelØnskes.setOnAction(e -> hotelØnskes());
 
         Label lblHoteller = new Label("Hoteller:");
-        pane.add(lblHoteller,0,2);
-        pane.add(lvwHoteller,0,3,2,6);
+        pane.add(lblHoteller, 0, 2);
+        pane.add(lvwHoteller, 0, 3, 2, 6);
         lvwHoteller.setMaxHeight(350);
         ChangeListener<Hotel> listener = (ov, o, n) -> this.hotelValg();
         lvwHoteller.getSelectionModel().selectedItemProperty().addListener(listener);
+        lvwHoteller.setDisable(true);
 
         Label lblHotelInfo = new Label("Hotel Info:");
-        pane.add(lblHotelInfo,2,3);
-        pane.add(txaHotelInfo,2,4,1,2);
+        pane.add(lblHotelInfo, 2, 3);
+        pane.add(txaHotelInfo, 2, 4, 1, 2);
         txaHotelInfo.setEditable(false);
         txaHotelInfo.setMaxHeight(75);
         txaHotelInfo.setMaxWidth(175);
+        txaHotelInfo.setDisable(true);
 
         Label lblHotelTilvalg = new Label("Hotel Tilvalg:");
-        pane.add(lblHotelTilvalg,2,6);
-        pane.add(lvwHotelTilvalg,2,7,1,2);
+        pane.add(lblHotelTilvalg, 2, 6);
+        pane.add(lvwHotelTilvalg, 2, 7, 1, 2);
         lvwHotelTilvalg.setMaxWidth(175);
+        lvwHotelTilvalg.setDisable(true);
 
 
         //================== Deltager og ledsager info ======================
 
         Label lblDeltagerStart = new Label("Deltager Fra");
         Label lblDeltagerTil = new Label("Til");
-        pane.add(lblDeltagerStart,3,0);
-        pane.add(lblDeltagerTil,4,0);
-        pane.add(dpDeltagerFra,3,1);
-        pane.add(dpDeltagerTil,4,1);
+        pane.add(lblDeltagerStart, 3, 0);
+        pane.add(lblDeltagerTil, 4, 0);
+        pane.add(dpDeltagerFra, 3, 1);
+        pane.add(dpDeltagerTil, 4, 1);
         dpDeltagerFra.setMaxWidth(115);
         dpDeltagerTil.setMaxWidth(115);
 
-        pane.add(cbxForedragsholder,3,2);
-        pane.add(cbxLedsager,3,3);
+        pane.add(cbxForedragsholder, 3, 2);
+        pane.add(cbxLedsager, 3, 3);
+        cbxLedsager.setOnAction(e -> ledsagerØnskes());
 
         GridPane ledsagerPane = new GridPane();
         Label lblLedsagerNavn = new Label("Ledsager Navn:");
-        ledsagerPane.add(lblLedsagerNavn,0,0);
-        ledsagerPane.add(txfLedsagerNavn,1,0);
+        ledsagerPane.add(lblLedsagerNavn, 0, 0);
+        ledsagerPane.add(txfLedsagerNavn, 1, 0);
         ledsagerPane.setHgap(10);
-        pane.add(ledsagerPane,3,4,2,1);
+        pane.add(ledsagerPane, 3, 4, 2, 1);
         txfLedsagerNavn.setMaxWidth(150);
+        txfLedsagerNavn.setDisable(true);
 
         //========= Udflugter ==========
 
         GridPane udflugtPane = new GridPane();
         Label lblUdflugter = new Label("Udflugter:");
         udflugtPane.setVgap(15);
-        udflugtPane.add(lblUdflugter,0,0);
-        udflugtPane.add(lvwUdflugter,0,1);
+        udflugtPane.add(lblUdflugter, 0, 0);
+        udflugtPane.add(lvwUdflugter, 0, 1);
+        lvwUdflugter.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         lvwUdflugter.setMaxHeight(150);
-        pane.add(udflugtPane,3,5,2,4);
+        pane.add(udflugtPane, 3, 5, 2, 4);
+        lvwUdflugter.setDisable(true);
 
 
         Label lblSamletPris = new Label("Samlet Pris:");
 
-        pane.add(lblSamletPris,2,9);
-        pane.add(txfSamletPris,3,9);
-        GridPane.setHalignment(lblSamletPris,HPos.RIGHT);
+        pane.add(lblSamletPris, 2, 9);
+        pane.add(txfSamletPris, 3, 9);
+        GridPane.setHalignment(lblSamletPris, HPos.RIGHT);
         txfSamletPris.setMaxWidth(100);
         Button btnOpretTilmelding = new Button("Opret Tilmelding");
-        pane.add(btnOpretTilmelding,4,9);
+        pane.add(btnOpretTilmelding, 4, 9);
 
         updateGui();
 
@@ -138,7 +148,7 @@ public class TilmeldingsVindue extends Application {
 
 
     //================ Helpers ====================
-    private void updateGui(){
+    private void updateGui() {
         konference = Storage.getKonferecer().getFirst();
         lvwHoteller.getItems().setAll(Storage.getHoteller());
         lvwUdflugter.getItems().setAll(konference.getUdflugter());
@@ -148,7 +158,7 @@ public class TilmeldingsVindue extends Application {
 
     //=================== Actions ================
 
-    private void hotelValg(){
+    private void hotelValg() {
         Hotel hotel = (Hotel) lvwHoteller.getSelectionModel().getSelectedItem();
         StringBuilder sb = new StringBuilder();
         sb.append(String.format(hotel.getNavn() + "\n"));
@@ -156,7 +166,27 @@ public class TilmeldingsVindue extends Application {
         sb.append(String.format("Pris Dobbelt: %.2f \n", hotel.getPrisDobbelt()));
         txaHotelInfo.setText(sb.toString());
         lvwHotelTilvalg.getItems().setAll(hotel.getHotelTilvalg());
+    }
 
+    private void hotelØnskes() {
+        boolean toggle = true;
+        if (cbxHotelØnskes.isSelected()) {
+            toggle = false;
+        }
+        dpCheckIn.setDisable(toggle);
+        dpCheckUd.setDisable(toggle);
+        lvwHoteller.setDisable(toggle);
+        lvwHotelTilvalg.setDisable(toggle);
+        txaHotelInfo.setDisable(toggle);
+    }
+
+    private void ledsagerØnskes(){
+        boolean toggle = true;
+        if (cbxLedsager.isSelected()){
+            toggle = false;
+        }
+        txfLedsagerNavn.setDisable(toggle);
+        lvwUdflugter.setDisable(toggle);
     }
 }
 
